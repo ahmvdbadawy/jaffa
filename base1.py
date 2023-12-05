a9418df33edaa27b9d3b0ead5d7b791199174ef1
AnasMady22 <110070707+AnasMady22@users.noreply.github.com>
2023-12-05 16:45:40 +0200

    Update base1.py

--- base1.py
+++ base1.py
@@ -1,61 +1,14 @@
 import tkinter as tk
+from tkinter import ttk
 from PIL import Image, ImageTk
 import heapq
 import random
 import time
-
+import wikipedia as wiki
+ 
 class GUI:
     def __init__(self, master:tk.Tk, background_image_path, From, To):
-        self.master = master
-        self.From = From
-        self.To = To
-        self.text_box_width = 383
-        self.text_box_x = 617
-        self.text_box_height = 1000
-        self.text_box_y = 0
-        self.master.title("Jaffa")
-        master.geometry('1000x1000')
-        # Margin and additions
-        Text_frame = tk.Text(master, bg="#2E4053", state=tk.DISABLED, font=("Verdana", 12), foreground="white", insertbackground="white", bd=3, relief=tk.SOLID, height=600, width=383)
-        Text_frame.place(x=self.text_box_x, y=self.text_box_y)
-        
-        # Input frame
-        input_frame = tk.LabelFrame(master, text="Direction", font=("bold", 14), labelanchor="n", bg="#3498DB", padx=10, pady=10, height=400, width=383)
-        input_frame.place(x=self.text_box_x, y=800)
-        
-        # Label and Entry for "To" city
-        
-        city_label = tk.Label(input_frame, text="To", bg="#3498DB", font=("bold", 12), foreground="white")
-        city_label.place(x=10, y=10)
-        
-        self.to_city_input = tk.Entry(input_frame, bg="#B3C9F4", justify='left', font=("Verdana", 12))
-        self.to_city_input.place(x=80, y=10)
-        
-        To_submit_button = tk.Button(input_frame, text="Submit", bg="#4E6D8C", fg="white", font=("Verdana", 12), command=self.submit_to_city, relief=tk.GROOVE)
-        To_submit_button.place(x=280, y=10)
-        
-        # Label and Entry for "From" city
-        
-        from_city_label = tk.Label(input_frame, text="From", bg="#3498DB", font=("bold", 12), foreground="white")
-        from_city_label.place(x=10, y=50)
-        
-        from_city_input = tk.Entry(input_frame, bg="#B3C9F4", justify='left', font=("Verdana", 12))
-        from_city_input.place(x=80, y=50)
-        
-        from_submit_button = tk.Button(input_frame, text="Submit", bg="#4E6D8C", fg="white", font=("Verdana", 12), command=self.submit_from_city, relief=tk.GROOVE)
-        from_submit_button.place(x=280, y=50)
-        
-        # Load and set the background image
-        self.background_image = Image.open(background_image_path)
-        self.background_photo = ImageTk.PhotoImage(self.background_image)
-
-        self.canvas = tk.Canvas(self.master, width=self.background_image.width, height=self.background_image.height)
-        # self.canvas.pack(expand=tk.YES, fill=tk.BOTH)
-        
-        self.canvas.place(width=self.background_image.width, height=self.background_image.height,x=0,y=0)
-        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.background_photo)
-        
-        
+                
         self.graph = {
             'Safad': ['Acre','Tiberias'],
             'Acre': ['Haifa','Nazareth','Tiberias','Safad'],
@@ -94,10 +47,70 @@ class GUI:
                     'Beersheba': (236,748),
                     'Gaza': (137,665.6)}
         
-        path = self.BFS(self.From, self.To)
-        print(path)
-        self.draw_lines(path)
-        self.draw_nodes()
+        self.master = master
+        self.From = From
+        self.To = To
+        self.text_box_width = 383
+        self.text_box_x = 617
+        self.text_box_height = 1000
+        self.text_box_y = 0
+        self.palestine_list = self._palestine_list()
+        self.Current_drawing = []
+        self.master.title("Jaffa")
+        master.geometry('1000x1000')
+        
+        # Margin and additions
+        Text_frame = tk.Text(master, bg="#2E4053", state=tk.DISABLED, font=("Verdana", 12), foreground="white", insertbackground="white", bd=3, relief=tk.SOLID, height=600, width=383)
+        Text_frame.place(x=self.text_box_x, y=self.text_box_y)
+        
+        # Input frame
+        input_frame = tk.LabelFrame(master, text="Direction", font=("bold", 14), labelanchor="n", bg="#3498DB", padx=10, pady=10, height=400, width=383)
+        input_frame.place(x=self.text_box_x, y=800)
+        
+        # Label and Entry for "To" city
+        city_label = tk.Label(input_frame,background='#3498DB', text="To", bg="#3498DB", font=("bold", 12), foreground="white")
+        city_label.place(x=10, y=10)
+
+        self.ans_from = from_combobox = ttk.Combobox(input_frame,width=30)
+        from_combobox['value'] = self.palestine_list
+        from_combobox.place(x=60, y=10)
+        
+        # self.to_city_input = tk.Entry(input_frame, bg="#B3C9F4", justify='left', font=("Verdana", 12))
+        # self.to_city_input.place(x=80, y=10)
+        
+        # Label and Entry for "From" city
+        
+        from_city_label = tk.Label(input_frame, text="From", bg="#3498DB", font=("bold", 12), foreground="white")
+        from_city_label.place(x=10, y=50)
+        
+        self.ans_to = to_combobox = ttk.Combobox(input_frame,width=30)
+        to_combobox['value'] = self.palestine_list
+        to_combobox.place(x=60, y=50)
+        
+        from_submit_button = tk.Button(input_frame, text="GO", bg="#4E6D8C", fg="white", font=("Verdana", 12), command=self.submit_path_city, relief=tk.GROOVE)
+        from_submit_button.place(x=220, y=85)
+        
+        # checkbox
+        
+        tk.Label(input_frame, text="search on wikipedia", bg="#3498DB", font=("bold", 12), foreground="white").place(x=35, y=90)
+        self.box = tk.Checkbutton(input_frame,bg="#3498DB").place(x=10, y=90)
+        
+        # ______________________________ خريطه فلسطين _______________________________
+        
+        self.background_image = Image.open(background_image_path)
+        self.background_photo = ImageTk.PhotoImage(self.background_image)
+
+        self.canvas = tk.Canvas(self.master, width=self.background_image.width, height=self.background_image.height)
+        # self.canvas.pack(expand=tk.YES, fill=tk.BOTH)
+        
+        self.canvas.place(width=self.background_image.width, height=self.background_image.height,x=0,y=0)
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.background_photo)
+        
+
+        # path = self.BFS(self.From, self.To)
+        # print(path)
+        # self.draw_lines(path)
+        # self.draw_nodes()
     
     def draw_nodes(self):
         for node, coordinates in self.nodes.items():
@@ -114,16 +127,19 @@ class GUI:
                 C = self.nodes['Acre-Haifa2']
                 D = self.nodes[list[x+1]]
                 line = self.canvas.create_line(A[0],A[1],B[0],B[1],C[0],C[1],D[0],D[1], fill="red", width=2)
+                self.Current_drawing.append(line)
             elif list[x] == 'Haifa' and list[x+1] == 'Acre':
                 A = self.nodes[list[x]]
                 B = self.nodes['Acre-Haifa2']
                 C = self.nodes['Acre-Haifa1']
                 D = self.nodes[list[x+1]]
                 line = self.canvas.create_line(A[0],A[1],B[0],B[1],C[0],C[1],D[0],D[1], fill="red", width=2)
+                self.Current_drawing.append(line)
             else:    
                 A = self.nodes[list[x]]
                 B = self.nodes[list[x+1]]
                 line = self.canvas.create_line(A[0],A[1],B[0],B[1], fill="red", width=2)
+                self.Current_drawing.append(line)
     def BFS(self, From, To):
         visited = []
         queue = [[From]]
@@ -143,19 +159,28 @@ class GUI:
                     queue.append(new_path)
             
     def random_color(self):
-         return "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-     
-     
-    def submit_to_city(self):
-        to_city = self.to_city_input.get()
-        print('city is : '+ to_city)
-        # Update this function based on your needs
-        
-    def submit_from_city(self):
-        from_city = self.from_city_input.get()
-        print('from city is : '+ from_city)
-        # add detiels anas
+        return "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    
+    def _palestine_list(self):
+        list = []
+        for x in self.graph:
+            list.append(x)
+        return list
+    
+    def clean_screen(self):
+        for x in self.Current_drawing:
+            self.canvas.delete(x)
+    
+    def submit_path_city(self):
+        self.From = self.ans_from.get()
+        self.To = self.ans_to.get()
+        lst = self.BFS(self.From,self.To)
+        if self.Current_drawing:
+            self.clean_screen()
+        self.draw_lines(lst)
+        # Anas, add details
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = GUI(root, "palestine_states.jpg",'Gaza','Tiberias')
-    root.mainloop() 
+    root.mainloop()
